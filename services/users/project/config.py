1a35398b41c6a87a23e2a4a0d6b5f6fa48bed41f
Bobby Watson <bobby.watson@bitsighttech.com>
2022-01-09 20:04:04 -0500

    Add bcrypt

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -6,12 +6,13 @@ import os
 
 class BaseConfig:
     """Base configuration"""
-
+    DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "my_precious"
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
+    BCRYPT_LOG_ROUNDS = 13
 
 
 class DevelopmentConfig(BaseConfig):
@@ -19,6 +20,7 @@ class DevelopmentConfig(BaseConfig):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     DEBUG_TB_ENABLED = True
+    BCRYPT_LOG_ROUNDS = 4
 
 
 class TestingConfig(BaseConfig):
@@ -26,9 +28,11 @@ class TestingConfig(BaseConfig):
 
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
+    BCRYPT_LOG_ROUNDS = 4
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
 
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
